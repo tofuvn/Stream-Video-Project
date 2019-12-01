df3f53fc60fc03deb46b41aa8bee64424bcce69b
The Ho <thehovnpl@gmail.com>
2019-12-01 01:19:15 +0100

    Init

---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {connect } from 'react-redux'
+import { fetchStream } from '../../actions'
+
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  render(){
+    return (
+      <div>StreamShow</div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow)
